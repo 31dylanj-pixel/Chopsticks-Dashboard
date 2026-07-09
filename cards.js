96eae472f6c0cc1e25f78a9bc992184143777740
31dylanj-pixel <31dylan_j@students.ics.ac.th>
2026-07-09 23:27:29 +0700

    Create cards.js

---
+++ cards.js
@@ -0,0 +1,47 @@
+const cards = {
+
+    common: [
+        ["🌿 Seaweed Card",20],
+        ["🐟 Minnow Card",25],
+        ["🍋 Lemon Card",25],
+        ["🎣 Fisherman Card",25],
+        ["🦀 Crab Card",25],
+        ["🐡 Pufferfish Card",25],
+        ["🐠 Baitfish Card",25],
+        ["⭐ Starfish Card",30]
+    ],
+
+    rare: [
+        ["🦑 Squid Card",100],
+        ["💎 Diamond Card",100],
+        ["🐙 Octopus Card",100],
+        ["🦞 Lobster Card",100],
+        ["🪼 Jellyfish Card",110],
+        ["⚡ Eel Card",120],
+        ["💡 Anglerfish Card",150],
+        ["🔱 Trident Card",200]
+    ],
+
+    epic: [
+        ["🦈 Shark Card",500],
+        ["🐋 Whale Card",500],
+        ["🌊 Tsunami Card",500],
+        ["🐙 Kraken Card",600],
+        ["⛈️ Storm Whale Card",650],
+        ["🪸 Coral Guardian Card",700]
+    ],
+
+    legendary: [
+        ["🐉 Leviathan Card",2000],
+        ["👑 Sea Emperor Card",2000],
+        ["🐲 Abyss Dragon Card",2500],
+        ["👁 Kraken Lord Card",3000]
+    ],
+
+    mythic: [
+        ["✨ Halo Card",7500],
+        ["🥢 Chopsticks God Card",10000],
+        ["🌌 Void Leviathan Card",12000],
+        ["🌊 Ocean Prime Card",15000]
+    ]
+};
