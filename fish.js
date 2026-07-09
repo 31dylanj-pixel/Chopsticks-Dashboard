a3b2248083e4926b542dba5072fefd3947e35a28
31dylanj-pixel <31dylan_j@students.ics.ac.th>
2026-07-09 17:19:08 +0700

    Part II of all fishes

--- fish.js
+++ fish.js
@@ -59,8 +59,47 @@ const fishTable = {
         ["🐟 Swordfish", 165],
         ["🦑 Electric Squid", 175],
         ["🐠 Coral Angelfish", 180]
-    ]
+    ],
 
+    
+    epic: [
+        ["🦑 Abyss Stalker", 300],
+        ["🐬 Porpoise Pup", 200],
+        ["🦈 Shark", 220],
+        ["🐬 Dolphin", 260],
+        ["🐠 Storm Ray", 360],
+        ["🦈 Reef Shark", 320],
+        ["🐙 Giant Octopus", 380],
+        ["🐠 Manta Ray", 420],
+        ["🦈 Deep Sea Hunter", 470],
+        ["🦈 Great White", 500],
+        ["🐋 Whale", 550],
+        ["🐋 Blue Titan Whale", 650],
+        ["🐙 Krakenling", 700]
+    ],
+
+    
+    legendary: [
+        ["🐉 Sea Serpent", 600],
+        ["🐉 Ember Serpent", 950],
+        ["👑 Golden Koi", 850],
+        ["🐉 Abyss Drakefish", 700],
+        ["👑 Crown Eel", 900],
+        ["👑 Abyssal Emperor Fish", 1300],
+        ["🐉 Storm Leviathan", 1200],
+        ["👑 Pearl Kingfish", 1500]
+    ],
+
+    
+    mythic: [
+        ["💎 Ocean Diamond", 1000],
+        ["🌠 Nebula Carp", 3000],
+        ["✨ Halo", 2500],
+        ["🌀 Rift Guardian Fish", 6000],
+        ["🌌 Void Eel", 5000],
+        ["💎 Crystal Seraph Fish", 8000],
+        ["🥢 Chopsticks Fish", 20000]
+    ],
 };
 
 const container = document.getElementById("fish-container");
