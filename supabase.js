9e39cf0ba8102bf8438c78ab771a30e799c1b4bd
31dylanj-pixel <31dylan_j@students.ics.ac.th>
2026-07-22 16:57:13 +0700

    Initialize Supabase client in supabase.js

---
+++ supabase.js
@@ -0,0 +1,9 @@
+const SUPABASE_URL = "https://rualkoaojvjiqsudzgah.supabase.co";
+
+const SUPABASE_ANON_KEY = "sb_publishable_oxTVjZfp9wvrrmG60Qm-cg_WsBD1pIE";
+
+
+const supabaseClient = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY
+);
