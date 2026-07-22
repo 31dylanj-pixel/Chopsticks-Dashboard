accf45ce7b407f1bf899309d2580d58ca1a1f540
31dylanj-pixel <31dylan_j@students.ics.ac.th>
2026-07-22 19:12:43 +0700

    Add togglePassword function for password visibility

--- account.js
+++ account.js
@@ -403,3 +403,22 @@ window.changePassword = async function(){
 
 
 updateAccount();
+
+window.togglePassword = function(inputId, icon){
+
+    const input =
+    document.getElementById(inputId);
+
+    if(input.type === "password"){
+
+        input.type = "text";
+        icon.textContent = "visibility_off";
+
+    } else {
+
+        input.type = "password";
+        icon.textContent = "visibility";
+
+    }
+
+};
